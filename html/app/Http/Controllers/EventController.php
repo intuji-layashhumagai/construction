5345fd194e2c3f12c522919f52de52bf06b424a3
intuji-layashhumagai <layash.humagai@intuji.com>
2025-11-17 10:11:39 +0000

    create a event controller class with store function

---
+++ html/app/Http/Controllers/EventController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEventRequest;
+use App\Models\EventStore;
+use App\Services\EventService;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+
+    public function __construct(private readonly EventService $eventService) {}
+        
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEventRequest $request)
+    {
+   
+       return $this->eventService->storeEvent($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EventStore $eventStore)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EventStore $eventStore)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EventStore $eventStore)
+    {
+        //
+    }
+}
