74f45e03a135e8c17fe599190fdb4b335d0043b4
layash humagai <layash.humagai@intuji.com>
2025-11-19 16:50:34 +0545

    create concurreent handeling action class

---
+++ html/app/Actions/Event/HandleConcurrentEventsAction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Actions\Event;
+
+use App\Models\Event;
+
+/**
+ * Handle events that occurred concurrently on different devices.
+ *
+ * When two events happen at the same logical time (concurrent), we need a
+ * deterministic way to decide which one takes precedence. We use a simple
+ * rule: the device with the lexicographically smaller ID wins.
+ */
+final class HandleConcurrentEventsAction
+{
+    public static function handle(array $incomingEventData, ?Event $mostRecentEvent): void
+    {
+
+        // Get the authoritative clock from the most recent event
+        $deviceClock = $incomingEventData['device_vector_clock'];
+        $authoritativeClock = $mostRecentEvent->vector_clock;
+
+        //todo: create action class from vectorservice functions
+        // Merge the clocks to create the new authoritative state
+        $mergedClock = app(\App\Services\VectorClockService::class)->merge($authoritativeClock, $deviceClock);
+
+        SaveEventToStoreAction::handle($incomingEventData, $mergedClock);
+    }
+}
