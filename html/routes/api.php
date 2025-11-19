45a36898d4defda2f150d7a2f59bff37335dc439
layash humagai <layash.humagai@intuji.com>
2025-11-19 16:44:47 +0545

    add api for sync operation

--- html/routes/api.php
+++ html/routes/api.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('events')->group(function () {
     // Store new event
-    Route::post('/', [EventController::class, 'store']);
+    Route::post('/', [EventController::class, 'store'])->name('storeEvent');
+    Route::post('/sync', [EventController::class, 'sync'])->name('syncEvent');
+
 });
