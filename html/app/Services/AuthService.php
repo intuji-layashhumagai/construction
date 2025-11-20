928c1dc04e181cd1c737f8861aeefe4b4160a408
layash humagai <layash.humagai@intuji.com>
2025-11-20 17:35:28 +0545

    create auth service to validate worker and generate certificates

---
+++ html/app/Services/AuthService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Actions\Auth\GenerateCertificateAction;
+use App\Actions\Worker\GetSingleWorkerAction;
+use Illuminate\Support\Facades\Hash;
+
+class AuthService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct() {}
+
+    public function login(array $request)
+    {
+        $worker = GetSingleWorkerAction::handle($request['email']);
+        if (! $worker || ! Hash::check($request['password'], $worker->password)) {
+
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
+        }
+
+        return GenerateCertificateAction::handle($worker);
+
+    }
+}
