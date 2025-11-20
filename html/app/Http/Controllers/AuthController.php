7e5ef0d2e83825c270147e9a417905a5d5c42e0d
layash humagai <layash.humagai@intuji.com>
2025-11-20 15:43:27 +0545

    create auth controller with login function

---
+++ html/app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Services\AuthService;
+
+class AuthController extends Controller
+{
+    public function __construct(private readonly AuthService $auth_service) {}
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function login(LoginRequest $request)
+    {
+        return $this->auth_service->login($request->validated());
+    }
+}
