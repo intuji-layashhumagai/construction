1f53d8dbb2c0ddd0749b74e3d0980271593a96f8
layash humagai <layash.humagai@intuji.com>
2025-11-20 10:13:40 +0545

    add jobs to the batch syncing

--- html/app/Services/SyncService.php
+++ html/app/Services/SyncService.php
@@ -2,14 +2,13 @@
 
 namespace App\Services;
 
-use App\Actions\Event\ProcessSingleEventAction;
+use App\Jobs\ProcessEventBatchJob;
 use Illuminate\Support\Facades\DB;
 
 class SyncService
 {
     public function __construct() {}
 
-    // todo: implementation of queue of background sync
     /**
      * Process a batch of events from an offline device.
      *
@@ -19,12 +18,12 @@ class SyncService
      */
     public function processEventBatch(array $eventsFromDevice): void
     {
-        // Wraped this entire batch in a database transaction
+        // Wrapped this entire batch in a database transaction
         // to ensure atomicity during conflict resolution and state updates
 
         DB::transaction(function () use ($eventsFromDevice) {
             foreach ($eventsFromDevice as $eventData) {
-                ProcessSingleEventAction::handle($eventData);
+                ProcessEventBatchJob::dispatch($eventData['device_id'], $eventData);
             }
         });
     }
