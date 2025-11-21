7bfb7fcfc62c09d3e04d18c1080898dce0c15aa0
layash humagai <layash.humagai@intuji.com>
2025-11-21 16:00:45 +0545

    update login request to include deviceId

--- html/app/Http/Requests/LoginRequest.php
+++ html/app/Http/Requests/LoginRequest.php
@@ -22,6 +22,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'deviceId' => 'required|uuid',
             'email' => 'required|email',
             'password' => 'required|string',
         ];
