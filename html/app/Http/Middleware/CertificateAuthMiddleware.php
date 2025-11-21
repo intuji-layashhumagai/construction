3a4e060b4959583f55a96943dbd71078292c3889
layash humagai <layash.humagai@intuji.com>
2025-11-21 12:31:09 +0545

    create certification verification middleware

---
+++ html/app/Http/Middleware/CertificateAuthMiddleware.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Actions\Auth\VerifyCertificateAction;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CertificateAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $certificatePem = $request->header('X-Client-Certificate');
+        $deviceId = $request->header('X-Device-ID');
+
+        if (! $certificatePem || ! $deviceId) {
+            return response()->json([
+                'error' => 'Certificate and device ID required',
+                'emergency_access_available' => true,
+            ], 401);
+        }
+        $cleanPemString = str_replace('\n', "\n", $certificatePem);
+
+        // Verify certificate
+        $verification = VerifyCertificateAction::handle($cleanPemString, $deviceId);
+        $workerId = $verification['certificate_info']['subject']['OU'];
+
+        if (! $verification['valid']) {
+            // Check if emergency access is allowed
+
+            $emergencyAllowed = VerifyCertificateAction::checkEmergencyAccess($workerId);
+
+            return response()->json([
+                'error' => 'Certificate verification failed',
+                'reason' => $verification['reason'],
+                'emergency_access_available' => $emergencyAllowed,
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
