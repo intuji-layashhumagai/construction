915d61de8be9b5097a889e2e773642cb71737b34
layash humagai <layash.humagai@intuji.com>
2025-11-25 13:20:22 +0545

    add controller function to initiate data sync process from session id

--- html/app/Http/Controllers/SyncController.php
+++ html/app/Http/Controllers/SyncController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Enums\SyncDirection;
+use App\Http\Requests\SyncEventProtocolRequest;
 use App\Http\Requests\SyncEventRequest;
 use App\Services\Sync\SyncProtocol;
+use App\Services\SyncService;
 
 class SyncController extends Controller
 {
-    public function __construct(private readonly SyncProtocol $syncProtocol) {}
+    public function __construct(private readonly SyncProtocol $syncProtocol, private readonly SyncService $syncService) {}
 
     public function sync(SyncEventRequest $request)
     {
         $deviceId = $request->validated()['events'][0]['device_id'];
         $workerId = $request->worker_id;
         $initiatedSync = $this->syncProtocol->initiateSync($deviceId, SyncDirection::UPLOAD, $workerId);
+
         return $initiatedSync;
+    }
+
+    public function syncData(SyncEventProtocolRequest $request, string $sessionId)
+    {
+        $syncData = $request->validated()['events'];
+        $processedSyncData = $this->syncService->processSyncData($sessionId, $syncData);
+
+        return $processedSyncData;
     }
 }
