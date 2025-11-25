805773ff8d9c4dd1aee3e2c28b0a611b771f23ff
layash humagai <layash.humagai@intuji.com>
2025-11-25 15:51:31 +0545

    update project config with sync constants

--- html/config/project.php
+++ html/config/project.php
@@ -13,4 +13,11 @@ return [
     ],
     'servers' => ['Server-A', 'Server-B', 'Server-C'],
     'cache_ttl' => env('CACHE_TTL', 2592000), // seconds
+    'sync' => [
+        'cache_ttl' => env('SYNC_CACHE_TTL', 300), // 5 minutes
+        'test_host' => env('SYNC_TEST_HOST', '8.8.8.8'),
+        'test_url' => env('SYNC_TEST_URL', 'https://www.google.com/favicon.ico'),
+        'test_size_bytes' => env('SYNC_TEST_SIZE_BYTES', 1000),
+        'bandwidth_cache_ttl' => env('SYNC_BANDWIDTH_CACHE_TTL', 1800), // 30 minutes
+    ],
 ];
