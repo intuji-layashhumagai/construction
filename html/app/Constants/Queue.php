390159c017f256c72ecbeb60ab32efdc41e42c95
layash humagai <layash.humagai@intuji.com>
2025-12-05 08:28:16 +0545

    create queue constants

---
+++ html/app/Constants/Queue.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constants;
+
+final class Queue
+{
+    public const HIGH_PERFORMANCE_SYNC = 'high_performance_sync';
+
+    public const DEFAULT = 'default';
+
+    public const MAIL = 'mail';
+
+    public const NOTIFICATIONS = 'notifications';
+}
