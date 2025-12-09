0d7ee43b098958d895b9f1231290d7600715dc4a
layash humagai <layash.humagai@intuji.com>
2025-12-09 09:53:30 +0545

    add controller and service for work sessions

---
+++ html/app/Http/Controllers/WorkSessionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkSession;
+use Illuminate\Http\Request;
+
+class WorkSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WorkSession $workSession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, WorkSession $workSession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WorkSession $workSession)
+    {
+        //
+    }
+}
